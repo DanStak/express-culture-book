b2cdfe5d625f4fb4b3dc1a40cbc06d4ba22c0c7c
Damian Stasiak <stas.damian95@gmail.com>
2023-10-26 11:48:33 +0200

    Added fallback to port env value

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import ExpressConfig from './Express/express.config'
 import dotenv from 'dotenv'
 dotenv.config() // Load environment variables from .env file
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 
 const app = ExpressConfig()
 

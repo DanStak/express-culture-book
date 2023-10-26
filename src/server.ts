ec0d28497909fc0234d4f81cd7e542352d91c65f
Damian Stasiak <stas.damian95@gmail.com>
2023-10-26 11:47:47 +0200

    Fix for modules

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import ExpressConfig from './Express/express.config.js'
+import ExpressConfig from './Express/express.config'
 
 import dotenv from 'dotenv'
 dotenv.config() // Load environment variables from .env file
@@ -6,9 +6,5 @@ dotenv.config() // Load environment variables from .env file
 const PORT = process.env.PORT
 
 const app = ExpressConfig()
-
-app.use((_req, _res, next) => {
-  next()
-})
 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`) })
